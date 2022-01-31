628a5ceaf3d41374383b0d17ae3fe880f6971fcc
erik <erik19970805@gmail.com>
2022-01-31 16:52:15 -0500

    Se activo el protocolo https y se añadio los certificados ssl para localhost

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import fs from 'fs';
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert/cert.pem')),
+      key: fs.readFileSync(path.resolve(__dirname, 'cert/key.pem')),
+    },
+  },
 });
