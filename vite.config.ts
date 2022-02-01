5406b7c6138c219192fdc330643afa5fa2242a8d
erik <erik19970805@gmail.com>
2022-02-01 09:29:18 -0500

    Se configuro los alias para poder utilizar @* haciendo refencia a diferentes paths y acortando las rutas de acceso

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 import fs from 'fs';
 import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert/cert.pem')),
-      key: fs.readFileSync(path.resolve(__dirname, 'cert/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, './cert/cert.pem')),
+      key: fs.readFileSync(path.resolve(__dirname, './cert/key.pem')),
+    },
+  },
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `$injectedColor: orange;`,
+      },
     },
   },
 });
